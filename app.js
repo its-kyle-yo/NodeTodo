1596e90bdd00a4caeb5e3884d7dccb36c62d3123
Kyle Thomas <kyle@schematical.com>
2017-06-07 08:55:21 -0500

    Adds in controller routes for API

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var app = express();
 var config = require('./config');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3001;
-
+var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 mongoose.connect(config.getDbConnectionString());
 app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
+apiController(app);
+setupController(app);
 app.listen(port);
