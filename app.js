34cfff93389b867977bce7355363e452d6d5cb51
Kyle Thomas <kyle@schematical.com>
2017-06-30 00:08:32 -0500

    Listing current routes on startup for ref.

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ var mongoose = require('mongoose');
 var port = process.env.PORT || 3001;
 var setupController = require('./controllers/setupController');
 var apiController = require('./controllers/apiController');
+
 mongoose.connect(config.getDbConnectionString());
-app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 apiController(app);
 setupController(app);
 app.listen(port);
+console.log(`App is live on localhost:${port}`);
+console.log('Try out one of these');
+
+app._router.stack.forEach(item => {
+    if(item.route !== undefined
+        && item.route.path !== undefined){
+            console.log(`${item.route.stack[0].method.toUpperCase()}: ${item.route.path}`);
+    }
+})
